819b36e71bc4e9462d7f5788d8e82abba4dd6f75
gabriel <gfelipe0555@gmail.com>
2026-03-12 17:07:24 -0300

    implementando o solid no meu projeto

---
+++ src/projeto_com_spring/demo/src/main/java/com/gablins/exceptions/InvalidCPFFormatException.java
@@ -0,0 +1,9 @@
+package com.gablins.exceptions;
+
+public class InvalidCPFFormatException extends RuntimeException {
+    public InvalidCPFFormatException(String msg)
+    {
+        super(msg);
+    }
+
+}
